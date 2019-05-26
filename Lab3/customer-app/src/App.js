f65ede1eb049efd98cfb5d4a0e0905e0723cb767
Amit Naik <amit.naik8103@gmail.com>
2019-05-26 11:59:33 +0530

    Update App.js

--- Lab3/customer-app/src/App.js
+++ Lab3/customer-app/src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    // with jsx
-    // <div className="App">
-    //   <h1>
-    //     My first react application
-    //   </h1>
-    // </div>
 
-    // without jsx 
-    // https://babeljs.io/repl converts jsx to javascript object
-    React.createElement("div", {
-      className: "App"
-    }, React.createElement("h1", null, "My first react application with babel"))
+  function PureFunction() {
+    return <div> Pure function </div>;
+  }
+
+  const FunctionES6 = () => {
+    return <div> Function with ES 6 </div>;
+  }
+
+  class ClassBasedComponent extends React.Component {
+    render() {
+      return <div> Class based component </div>;
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Types of component</h1>
+      <PureFunction />
+      <FunctionES6 />
+      <ClassBasedComponent />
+    </div>
   );
 }
 
