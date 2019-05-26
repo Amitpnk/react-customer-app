88415201cb0d3ef930f12992d04cd29a30e9503b
Amit Naik <amit.naik8103@gmail.com>
2019-05-26 14:42:28 +0530

    lab 10 Organize the project and adding add & delete event to table

---
+++ Lab10/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Customer from '../Customer/Customer'
+
+function App() {
+  return (
+    <div >
+      <Customer />
+    </div>
+  );
+}
+
+export default App;
